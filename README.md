1ff3a40cf110150401aa2a76d319fa3d3f374b97
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-07-17 02:22:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# KC-ECELL-NodeJS-Server
---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(process.env.SERVER_PORT, () => console.log('Server is running on ' + process.env.SERVER_PORT));
